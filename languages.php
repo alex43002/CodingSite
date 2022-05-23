37b1c76e6973194d13461539382d761961d6fc74
Alexander James DeJesus <alexdejesus336@Yahoo.com>
2022-05-23 03:58:38 -0400

    Language direction Major Change
    Added Mock-up raw data files into folder directories along with site data

--- languages.php
+++ languages.php
@@ -4,61 +4,161 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=1920, maximum-scale=1.0" />
     <link rel="shortcut icon" type="image/png" href="https://animaproject.s3.amazonaws.com/home/favicon.png" />
-    <link rel="stylesheet" type="text/css" href="/pages/languages/language-main-site.css" />
-    <link rel="stylesheet" type="text/css" href="/pages/languages/styleguide.css" />
-    <link rel="stylesheet" type="text/css" href="/pages/languages/globals.css" />
+    <meta name="og:type" content="website" />
+    <meta name="twitter:card" content="photo" />
+    <link rel="stylesheet" type="text/css" href="css/language-main-site.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://animaapp.s3.amazonaws.com/launchpad-static/launchpad.js"></script>
+    <link rel="stylesheet" type="text/css" href="/pages/languages/css/styleguide.css" />
+    <link rel="stylesheet" type="text/css" href="/pages/languages/css/globals.css" />
   </head>
   <body style="margin: 0; background: #ffffff">
     <input type="hidden" id="anPageName" name="page" value="language-main-site" />
     <div class="container-center-horizontal">
-      <div class="language-main-site screen">
-        <div class="overlap-group1">
-          <img
-            class="line-1"
-            src="https://anima-uploads.s3.amazonaws.com/projects/6269c75d2bed8db80d8afd0c/releases/6269f7db13a5b6805ad236b5/img/line-1-1x-png@1x.png"
-          />
-          <img
-            class="line-2"
-            src="https://anima-uploads.s3.amazonaws.com/projects/6269c75d2bed8db80d8afd0c/releases/6269f7db13a5b6805ad236b5/img/line-2@1x.png"
-          />
-          <div class="footer-group border-1px-dove-gray">
-            <div class="personal"><h1 class="title arial-regular-normal-white-25px">About me</h1></div>
-            <div class="up">
-              <div class="overlap-group">
-                <div class="component-6-1"></div>
-                <div class="rectangle-130"></div>
-                <div class="arrow-up-1"></div>
-              </div>
-            </div>
-            <div class="socials">
-              <div class="email-me-instagram-git-hub-docker arial-regular-normal-white-25px">
-                Email Me&nbsp;&nbsp; Instagram&nbsp;&nbsp; GitHub&nbsp;&nbsp; Docker
-              </div>
-            </div>
+      <div class="language-main-site animate-enter screen">
+        <div class="overlap-group2">
+          <div class="footer-group">
+            <a href="https://example.com/"><div class="about-me arial-regular-normal-white-16px">About me</div> </a
+            ><a href="#header" class="align-self-flex-start">
+              <div class="up">
+                <div class="overlap-group">
+                  <div class="component-6-1"></div>
+                  <div class="rectangle-130"></div>
+                  <div class="component-1"></div>
+                </div></div></a
+            ><a href="https://example.com/"><div class="email-me arial-regular-normal-white-16px">Email Me</div> </a
+            ><a href="https://example.com/"><div class="instagram arial-regular-normal-white-16px">Instagram</div> </a
+            ><a href="https://example.com/"><div class="git-hub arial-regular-normal-white-16px">GitHub</div> </a
+            ><a href="https://example.com/"><div class="docker arial-regular-normal-white-16px">Docker</div> </a>
           </div>
           <div class="main-body"></div>
-          <div class="header border-1px-dove-gray">
-            <div class="main-tabs">
-              <div class="main-tabs-1 arial-regular-normal-white-25px">
-                Home&nbsp;&nbsp; Languages&nbsp;&nbsp; Career Specializations&nbsp;&nbsp; Help
-              </div>
-            </div>
-            <div class="component-1-2"><div class="login arial-regular-normal-white-25px">Login</div></div>
-          </div>
-          <img
-            class="image-1"
-            src="https://anima-uploads.s3.amazonaws.com/projects/6269c75d2bed8db80d8afd0c/releases/626a4145cc9662191f2e9d13/img/image-1@1x.png"
-          />
-          <div class="group-1">
+          <img class="image-1" src="/pages/languages/img/image-1-1x-png@1x.png" />
+          <div class="language-list-component">
             <div class="menu"></div>
             <div class="languages">Languages</div>
           </div>
-          <div class="group-2">
+          <img class="line-2" src="/pages/languages/img/line-1-1x-png@1x.png" />
+          <div class="concept-list-component">
             <div class="menu"></div>
-            <div class="concepts">Concepts</div>
+            <div class="languages">Languages</div>
+          </div>
+        </div>
+        <div class="header" id="header">
+          <div class="overlap-group1">
+            <div class="header-1"></div>
+            <img class="line-1" src="/pages/languages/img/line-1-1x-png@1x.png" />
+            <a href="https://example.com/"><div class="help arial-regular-normal-white-16px">Help</div> </a
+            ><a href="https://example.com/"
+              ><div class="specialization arial-regular-normal-white-16px">Specializations</div> </a
+            ><a href="https://example.com/"><div class="career arial-regular-normal-white-16px">Career</div> </a
+            ><a href="https://example.com/"><div class="languages-1 arial-regular-normal-white-16px">Languages</div> </a
+            ><a href="https://example.com/"><div class="place arial-regular-normal-white-16px">Home</div> </a
+            ><a href="https://example.com/"><div class="login arial-regular-normal-white-16px">Login</div> </a>
           </div>
         </div>
       </div>
     </div>
+    <script>
+      function ShowOnScroll() {
+        this.toShow = [];
+        this.nextEventY = undefined;
+      }
+
+      ShowOnScroll.prototype.show = function (e) {
+        e.style.display = "";
+      };
+
+      ShowOnScroll.prototype.hide = function (e) {
+        e.style.display = "none";
+      };
+
+      ShowOnScroll.prototype.getTop = function (e) {
+        if (e.Top != undefined && e.Top != 0) {
+          return e.Top;
+        }
+        var top = 0;
+        var iter = e;
+        do {
+          top += iter.offsetTop || 0;
+          iter = iter.offsetParent;
+        } while (iter);
+        e.Top = top;
+        return top;
+      };
+
+      ShowOnScroll.prototype.onScroll = function () {
+        var screenBottom = window.pageYOffset + window.innerHeight;
+        if (this.nextEventY == undefined || this.nextEventY > screenBottom) {
+          return;
+        }
+        this.nextEventY = undefined;
+        for (var i = 0; i < this.toShow.length; i++) {
+          var e = this.toShow[i];
+          var top = this.getTop(e);
+          if (top < screenBottom) {
+            this.show(e);
+            this.toShow.shift();
+            i--;
+          } else {
+            this.nextEventY = top;
+            break;
+          }
+        }
+      };
+
+      ShowOnScroll.prototype.resetScrolling = function () {
+        // Clear state
+        var screenBottom = window.pageYOffset + window.innerHeight;
+        for (var i = 0; i < this.toShow.length; i++) {
+          var e = this.toShow[i];
+          this.show(e);
+        }
+        this.toShow = [];
+        this.nextEventY == undefined;
+
+        // Collect items
+        var itemsToShowOnScroll = Array.prototype.slice.call(document.getElementsByTagName("*"));
+        itemsToShowOnScroll = itemsToShowOnScroll.filter(function (e) {
+          return e.getAttribute("show-on-scroll") != undefined;
+        });
+        var getTop = this.getTop;
+        itemsToShowOnScroll.sort(function (a, b) {
+          return getTop(a) - getTop(b);
+        });
+        for (var i = 0; i < itemsToShowOnScroll.length; i++) {
+          var e = itemsToShowOnScroll[i];
+          var top = this.getTop(e);
+          if (top < screenBottom) {
+            continue;
+          }
+          this.toShow.push(e);
+          this.hide(e);
+          this.nextEventY = this.nextEventY != undefined ? this.nextEventY : top;
+        }
+      };
+
+      ShowOnScroll.prototype.handleEvent = function (e) {
+        switch (e.type) {
+          case "scroll":
+            this.onScroll();
+            break;
+          case "resize":
+            this.resetScrolling();
+            break;
+        }
+      };
+
+      ShowOnScroll.prototype.init = function () {
+        this.resetScrolling();
+        window.addEventListener("scroll", this);
+        window.addEventListener("resize", this);
+      };
+
+      // After anima-src
+      setTimeout(function () {
+        var instShowOnScroll = new ShowOnScroll();
+        instShowOnScroll.init();
+      }, 250);
+    </script>
   </body>
 </html>
