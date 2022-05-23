00ffec3823e9d0a8e516d09ea941e6fa46fded98
Alexander James DeJesus <alexdejesus336@Yahoo.com>
2022-05-23 02:30:32 -0400

    Reinstate Home Mock-up

--- home.php
+++ home.php
@@ -1,88 +1,118 @@
 <!DOCTYPE html>
 <html>
+  <head>
+    <meta charset="utf-8" http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width = device-width, initial-scale = 1" />
 
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=1920, maximum-scale=1.0" />
     <link rel="shortcut icon" type="image/png" href="https://animaproject.s3.amazonaws.com/home/favicon.png" />
     <link rel="stylesheet" type="text/css" href="/pages/home/home.css" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
     <script src="https://animaapp.s3.amazonaws.com/launchpad-static/launchpad.js"></script>
     <link rel="stylesheet" type="text/css" href="/pages/home/styleguide.css" />
     <link rel="stylesheet" type="text/css" href="/pages/home/globals.css" />
-</head>
-
-<body style="margin: 0; background: #ffffff; width: 100%;">
-    <input type="hidden" id="anPageName" name="page" value="home" />
-    <div class="container-center-horizontal">
-        <div class="home animate-enter screen">
-            <div class="overlap-group2">
-                <div class="footer-group border-1px-dove-gray">
-                    <a href="https://www.linkedin.com/in/alexander-dejesus-184ba2226/">
-                        <div class="about-me arial-regular-normal-white-16px">LinkedIn</div>
-                    </a>
-                    <a href="#header" class="align-self-flex-start">
-                        <div class="up">
-                            <div class="overlap-group">
-                                <div class="component-6-2"></div>
-                                <div class="rectangle-130 border-2px-sail"></div>
-                                <div class="component-1"></div>
+  </head>
+    <body style="margin: 0; background: #ffffff">
+        <input type="hidden" id="anPageName" name="page" value="home" />
+        <div class="container-center-horizontal">
+            <div class="home animate-enter screen">
+                <div class="overlap-group2">
+                    <div class="footer-group border-1px-dove-gray">
+                        <a href="https://example.com/">
+                            <div class="about-me arial-regular-normal-white-16px">
+                                About me
+                            </div> 
+                        </a>
+                        <a href="#header" class="align-self-flex-start">
+                            <div class="up">
+                                <div class="overlap-group">
+                                    <div class="component-6-2">
+                                    </div>
+                                    <div class="rectangle-130 border-2px-sail">
+                                    </div>
+                                    <div class="component-1">
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </a>
-                    <a href="mailto:alexdejesus336@yahoo.com?subject = Questions or Feedback">
-                        <div class="email-me arial-regular-normal-white-16px">Email Me</div>
-                    </a>
-                    <a href="https://www.instagram.com/codinghelpforeveryone/">
-                        <div class="instagram arial-regular-normal-white-16px">Instagram</div>
-                    </a>
-                    <a href="https://github.com/alex43002?tab=repositories">
-                        <div class="git-hub arial-regular-normal-white-16px">GitHub</div>
-                    </a><a href="https://hub.docker.com/u/ajd27">
-                        <div class="docker arial-regular-normal-white-16px">Docker</div>
-                    </a>
+                        </a>
+                        <a href="https://example.com/">
+                            <div class="email-me arial-regular-normal-white-16px">
+                                Email Me
+                            </div> 
+                        </a>            
+                        <a href="https://example.com/">
+                            <div class="instagram arial-regular-normal-white-16px">
+                                Instagram
+                            </div> 
+                        </a>
+                        <a href="https://example.com/">
+                            <div class="git-hub arial-regular-normal-white-16px">
+                                GitHub
+                            </div> 
+                        </a>           
+                        <a href="https://example.com/">
+                            <div class="docker arial-regular-normal-white-16px">
+                                Docker
+                            </div> 
+                        </a>
+                    </div>
+                    <div class="main-body">
+
+                    </div>
+                    <img
+                        class="image-1"
+                        src="https://anima-uploads.s3.amazonaws.com/projects/6269c75d2bed8db80d8afd0c/releases/6289cd28adcdb6e54a3da35b/img/image-1-1x-png-1x-png@1x.png"
+                    />
+                    <img
+                        class="line-2"
+                        src="https://anima-uploads.s3.amazonaws.com/projects/6269c75d2bed8db80d8afd0c/releases/627887b2b1558fc4f89153bc/img/line-1-1x-png@1x.png"
+                    />
                 </div>
-                <div class="main-body"></div>
-                <img class="image-1" src="https://anima-uploads.s3.amazonaws.com/projects/6269c75d2bed8db80d8afd0c/releases/6289cd28adcdb6e54a3da35b/img/image-1-1x-png-1x-png@1x.png" />
-                <img class="line-2" src="https://anima-uploads.s3.amazonaws.com/projects/6269c75d2bed8db80d8afd0c/releases/627887b2b1558fc4f89153bc/img/line-1-1x-png@1x.png" />
-            </div>
+                
+                <div class="header" id="header">
+                    <div class="overlap-group1">
+                        <div class="header-1 border-1px-dove-gray">
 
-            <!-- Side Menu Plugin Placement -->
-            <div id="SidePanel" class="sidepanel" style="width:7px; bottom:0;">
-                <div class="PanelDoor" onclick="sideBarFunctionality(); scrollLink();"></div>
-                <a href="#First">Welcome</a>
-                <a href="#List">General Topics</a>
-            </div>
-
-            <!-- End of side menu -->
-
-            <div class="header" id="header">
-                <div class="overlap-group1">
-                    <div class="header-1 border-1px-dove-gray"></div>
-                    <img class="line-1" src="https://anima-uploads.s3.amazonaws.com/projects/6269c75d2bed8db80d8afd0c/releases/627887b2b1558fc4f89153bc/img/line-1-1x-png@1x.png" />
-                    <a href="help">
-                        <div class="help arial-regular-normal-white-16px">Help</div>
-                    </a>
-                    <a href="specializations">
-                        <div class="specialization arial-regular-normal-white-16px">Specializations</div>
-                    </a>
-                    <a href="career">
-                        <div class="career arial-regular-normal-white-16px">Career</div>
-                    </a>
-                    <a href="languages">
-                        <div class="languages arial-regular-normal-white-16px">Languages</div>
-                    </a>
-                    <a href="home">
-                        <div class="place arial-regular-normal-white-16px">Home</div>
-                    </a>
-                    <a href="https://example.com/">
-                        <div class="login arial-regular-normal-white-16px">Login</div>
-                    </a>
+                        </div>
+                        <img
+                            class="line-1"
+                            src="https://anima-uploads.s3.amazonaws.com/projects/6269c75d2bed8db80d8afd0c/releases/627887b2b1558fc4f89153bc/img/line-1-1x-png@1x.png"
+                        />
+                        <a href="https://example.com/">
+                            <div class="help arial-regular-normal-white-16px">
+                                Help
+                            </div> 
+                        </a>
+                        <a href="https://example.com/">
+                            <div class="specialization arial-regular-normal-white-16px">
+                                Specializations
+                            </div>
+                        </a>
+                        <a href="https://example.com/">
+                            <div class="career arial-regular-normal-white-16px">
+                                Career
+                            </div> 
+                        </a>
+                        <a href="https://example.com/">
+                            <div class="languages arial-regular-normal-white-16px">
+                                Languages
+                            </div> 
+                        </a>
+                        <a href="https://example.com/">
+                            <div class="place arial-regular-normal-white-16px">
+                                Home
+                            </div> 
+                        </a>
+                        <a href="https://example.com/">
+                            <div class="login arial-regular-normal-white-16px">
+                                Login
+                            </div> 
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <script type="text/javascript" src="/pages/home/AllFunctions.js"></script>
-</body>
+        <script type="text/javascript" src="/pages/home/AllFunctions.js">
 
+        </script>
+    </body>
 </html>
