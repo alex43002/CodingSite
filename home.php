bc408e1b5ffa859e43360df882d408cb6a4131c7
Alexander James DeJesus <alexdejesus336@Yahoo.com>
2022-05-25 03:56:21 -0400

    Major Change
    Finished the minor revisions of the Home Page and used that as the base template for the rest of the major sections. Moved creator file to experimental for testing later

--- home.php
+++ home.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8" http-equiv="Content-type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width = device-width, initial-scale = 1" />
+    <head>
+        <meta charset="utf-8" http-equiv="Content-type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width = device-width, initial-scale = 1" />
 
-    <link rel="shortcut icon" type="image/png" href="https://animaproject.s3.amazonaws.com/home/favicon.png" />
-    <link rel="stylesheet" type="text/css" href="/pages/home/home.css" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-    <script src="https://animaapp.s3.amazonaws.com/launchpad-static/launchpad.js"></script>
-    <link rel="stylesheet" type="text/css" href="/pages/home/styleguide.css" />
-    <link rel="stylesheet" type="text/css" href="/pages/home/globals.css" />
-  </head>
+        <link rel="shortcut icon" type="image/png" href="https://animaproject.s3.amazonaws.com/home/favicon.png" />
+        <link rel="stylesheet" type="text/css" href="/pages/home/home.css" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+        <script src="https://animaapp.s3.amazonaws.com/launchpad-static/launchpad.js"></script>
+        <link rel="stylesheet" type="text/css" href="/pages/home/styleguide.css" />
+        <link rel="stylesheet" type="text/css" href="/pages/home/globals.css" />
+    </head>
     <body style="margin: 0; background: #ffffff">
         <input type="hidden" id="anPageName" name="page" value="home" />
         <div class="container-center-horizontal">
@@ -65,13 +65,14 @@
                         class="image-1"
                         src="https://anima-uploads.s3.amazonaws.com/projects/6269c75d2bed8db80d8afd0c/releases/6289cd28adcdb6e54a3da35b/img/image-1-1x-png-1x-png@1x.png"
                     />
+                    
+                </div>
+                <!-- The Footer -->
+                <div class="footer-group border-1px-dove-gray">
                     <img
                         class="line-2"
                         src="https://anima-uploads.s3.amazonaws.com/projects/6269c75d2bed8db80d8afd0c/releases/627887b2b1558fc4f89153bc/img/line-1-1x-png@1x.png"
                     />
-                </div>
-                <!-- The Footer -->
-                <div class="footer-group border-1px-dove-gray">
                     <a href="https://www.linkedin.com/in/alexander-dejesus-184ba2226/">
                         <div class="about-me arial-regular-normal-white-16px">
                             About me
--- specializations.php
+++ specializations.php
@@ -1,12 +1,121 @@
 <!DOCTYPE html>
-<html lang="en-US">
+<html>
+    <head>
+        <meta charset="utf-8" http-equiv="Content-type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width = device-width, initial-scale = 1" />
 
-<head>
-    <title>
-       About Me
-    </title>
-</head>
+        <link rel="shortcut icon" type="image/png" href="https://animaproject.s3.amazonaws.com/home/favicon.png" />
+        <link rel="stylesheet" type="text/css" href="/pages/specializations/home.css" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+        <script src="https://animaapp.s3.amazonaws.com/launchpad-static/launchpad.js"></script>
+        <link rel="stylesheet" type="text/css" href="/pages/specializations/styleguide.css" />
+        <link rel="stylesheet" type="text/css" href="/pages/specializations/globals.css" />
+    </head>
+    <body style="margin: 0; background: #ffffff">
+        <input type="hidden" id="anPageName" name="page" value="home" />
+        <div class="container-center-horizontal">
+            <div class="home animate-enter screen">
+                <div class="header" id="header">
+                    <div class="overlap-group1">
+                        <div class="header-1 border-1px-dove-gray">
 
-<body>
-    WIP
-</body>
+                        </div>
+                        <img
+                            class="line-1"
+                            src="https://anima-uploads.s3.amazonaws.com/projects/6269c75d2bed8db80d8afd0c/releases/627887b2b1558fc4f89153bc/img/line-1-1x-png@1x.png"
+                        />
+                        <a href="help">
+                            <div class="help arial-regular-normal-white-16px">
+                                Help
+                            </div> 
+                        </a>
+                        <a href="specializations">
+                            <div class="specialization arial-regular-normal-white-16px">
+                                Specializations
+                            </div>
+                        </a>
+                        <a href="career">
+                            <div class="career arial-regular-normal-white-16px">
+                                Career
+                            </div> 
+                        </a>
+                        <a href="languages">
+                            <div class="languages arial-regular-normal-white-16px">
+                                Languages
+                            </div> 
+                        </a>
+                        <a href="home">
+                            <div class="place arial-regular-normal-white-16px">
+                                Home
+                            </div> 
+                        </a>
+                        <a href="https://example.com/">
+                            <div class="login arial-regular-normal-white-16px">
+                                Login
+                            </div> 
+                        </a>
+                    </div>
+                </div>
+                <!-- Everything below the header -->
+                <div class="overlap-group2">
+                    <div class="main-body">
+
+                    </div>
+                    <img
+                        class="image-1"
+                        src="https://anima-uploads.s3.amazonaws.com/projects/6269c75d2bed8db80d8afd0c/releases/6289cd28adcdb6e54a3da35b/img/image-1-1x-png-1x-png@1x.png"
+                    />
+                    
+                </div>
+                <!-- The Footer -->
+                <div class="footer-group border-1px-dove-gray">
+                    <img
+                        class="line-2"
+                        src="https://anima-uploads.s3.amazonaws.com/projects/6269c75d2bed8db80d8afd0c/releases/627887b2b1558fc4f89153bc/img/line-1-1x-png@1x.png"
+                    />
+                    <a href="https://www.linkedin.com/in/alexander-dejesus-184ba2226/">
+                        <div class="about-me arial-regular-normal-white-16px">
+                            About me
+                        </div> 
+                    </a>
+                    <a href="#header" class="align-self-flex-start">
+                        <div class="up">
+                            <div class="overlap-group">
+                                <div class="component-6-2">
+                                </div>
+                                <div class="rectangle-130 border-2px-sail">
+                                </div>
+                                <div class="component-1">
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                    <a href="mailto:alexdejesus336@yahoo.com?subject = Questions or Feedback">
+                        <div class="email-me arial-regular-normal-white-16px">
+                            Email Me
+                        </div> 
+                    </a>            
+                    <a href="https://www.instagram.com/codinghelpforeveryone/">
+                        <div class="instagram arial-regular-normal-white-16px">
+                            Instagram
+                        </div> 
+                    </a>
+                    <a href="https://github.com/alex43002?tab=repositories">
+                        <div class="git-hub arial-regular-normal-white-16px">
+                            GitHub
+                        </div> 
+                    </a>           
+                    <a href="https://hub.docker.com/u/ajd27">
+                        <div class="docker arial-regular-normal-white-16px">
+                            Docker
+                        </div> 
+                    </a>
+                </div>
+                <!-- Footer end -->    
+            </div>
+        </div>
+        <script type="text/javascript" src="/pages/specializations/AllFunctions.js">
+
+        </script>
+    </body>
+</html>
